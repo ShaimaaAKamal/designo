867d0f225d52370dbf10553b505b7cbe2e75f2e3
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-28 15:32:54 +0200

    edit app import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import App from './App';
+import App from './Components/App/App'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 import reportWebVitals from './reportWebVitals';
