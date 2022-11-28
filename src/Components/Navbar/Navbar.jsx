c73b16fbbaf1fcbafc60d48d1393130703396640
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-28 15:34:32 +0200

    init navbar implemenation

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import logo from '../../assets/shared/desktop/logo-dark.png';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <div>
-      Navbar
+   <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <Link class="navbar-brand" to="">
+      <img src={logo} alt='logo'/>
+    </Link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <Link class="nav-link text-capitalize active" aria-current="page" to="About">our company</Link>
+        </li>
+        <li class="nav-item">
+          <Link class="nav-link text-capitalize" to="/locations">locations</Link>
+        </li>
+         <li class="nav-item">
+          <Link class="nav-link text-capitalize" to="/contact">contact</Link>
+        </li>
+      </ul>
     </div>
+  </div>
+</nav>
   )
 }
