aa9a2f2e2cee8193e430600aa849ff5504df0448
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-29 13:52:20 +0200

    initation app routes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,18 @@
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from '../Layout/Layout';
+import NotFound from '../NotFound/NotFound';
+import Home from '../Home/Home'
 
 function App() {
+   const routes=createBrowserRouter([
+      {path:'',element:<Layout/>,children:[
+     {path:'/',element:<Home/>}
+    ]}
+   ]
+   )
   return (
     <div className="App">
-        <h1>App</h1>
+      <RouterProvider router={routes}></RouterProvider>
     </div>
   );
 }
