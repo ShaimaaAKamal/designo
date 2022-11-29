01a967b19b21b720540cefe8155e341cf72e0b0a
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-29 14:50:07 +0200

    add footerComponent to layout

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Navbar/Navbar';
+import FooterComp from '../FooterComp/FooterComp';
 
 export default function Layout() {
   return (
     <>
       <Navbar/>
       <Outlet/>
+      <FooterComp/>
     </>
   )
 }
