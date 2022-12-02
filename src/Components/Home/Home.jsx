215217fff9fc5dcd107af5cf7fb0d832a9f98f0a
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-02 20:24:13 +0200

    add home cards component to home component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import HomeHeading from '../HomeHeading/HomeHeading';
+import HomeCards from '../HomeCards/HomeCards';
 
 export default function Home() {
   return (
     <div className='container-lg'>
       <HomeHeading/>
+      <HomeCards/>
     </div>
   )
 }
