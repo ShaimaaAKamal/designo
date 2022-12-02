d4521093fc1e7a141faa3aa5b4ba594ad419ba70
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-02 20:57:33 +0200

    implement resource component and append it to home component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HomeHeading from '../HomeHeading/HomeHeading';
 import HomeCards from '../HomeCards/HomeCards';
+import HomeResource from '../HomeResource/HomeResource';
 
 export default function Home() {
   return (
     <div className='container-lg'>
       <HomeHeading/>
       <HomeCards/>
+      <HomeResource/>
     </div>
   )
 }
