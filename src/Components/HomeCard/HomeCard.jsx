08e8c30ab2883c91cbcfcb390d66c8adc522ecc4
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-02 20:22:53 +0200

    implement home card component

---
+++ src/Components/HomeCard/HomeCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+export default function HomeCard({Large,Medium,Small,title,route}) {
+  return (
+     <div className='position-relative rounded-3'>
+                <img src={Large} alt="web image" className='w-100 rounded-3 imgLarge' />
+                <img src={Medium} alt="web image" className='w-100 rounded-3 imgMedium' />
+                <img src={Small} alt="web image" className='w-100 rounded-3 imgSmall' />
+                <div className='rounded-3 position-absolute w-100 h-100 top-0 d-flex align-items-center justify-content-center hoverCard'>
+                    <div className='text-white text-center'>
+                        <h6 className='display-6 text-uppercase hoverCardTitle'>{title}</h6>
+                         <Link to={route} className='fs-sm text-uppercase text-decoration-none text-reset'>View Projects</Link>
+                    </div>
+                </div>
+    </div>
+  )
+}
