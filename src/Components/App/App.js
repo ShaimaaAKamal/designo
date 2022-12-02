7ae647e3b1363be316e4fa6e97b0247e28ad9fef
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-02 20:23:22 +0200

    update site routes by adding new routes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,18 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from '../Layout/Layout';
 import NotFound from '../NotFound/NotFound';
-import Home from '../Home/Home'
+import Home from '../Home/Home';
+import WebDesign from '../WebDesign/WebDesign';
+import AppDesign from '../AppDesign/AppDesign';
+import GraphicDesign from '../GraphicDesign/GraphicDesign';
 
 function App() {
    const routes=createBrowserRouter([
-      {path:'',element:<Layout/>,children:[
-     {path:'/',element:<Home/>}
+      {path:'',element:<Layout/>,errorElement:<NotFound/>,children:[
+     {index:true,element:<Home/>},
+     {path:'web',element:<WebDesign/>},
+     {path:'app',element:<AppDesign/>},
+     {path:'graphic',element:<GraphicDesign/>}
     ]}
    ]
    )
