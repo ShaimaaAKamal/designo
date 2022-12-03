95b1a6a678fe18d647da89cf7a2b1dc63ba82c65
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-03 16:44:54 +0200

    add images

--- src/Components/WebDesign/WebDesign.jsx
+++ src/Components/WebDesign/WebDesign.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import DesignHeading from '../DesgnHeading/DesignHeading';
 import DesignCards from '../DesignCards/DesignCards';
 import importAll from '../../js/images';
+import HomeCard from '../HomeCard/HomeCard';
+import LetsTalk from '../LetsTalk/LetsTalk';
+import appLarge from '../../assets/home/desktop/image-app-design.jpg';
+import graphic from '../../assets/home/desktop/image-graphic-design.jpg';
+import appMedium from '../../assets/home/tablet/image-app-design.jpg';
+import graphicMedium from '../../assets/home/tablet/image-graphic-design.jpg';
+import appSmall from '../../assets/home/mobile/image-app-design.jpg';
+import graphicSmall from '../../assets/home/mobile/image-graphic-design.jpg';
 
 export default function WebDesign() {
    const designTitles=['EXPRESS','TRANSFER','PHOTON','BUILDER','BLOGR','CAMP']
@@ -16,6 +24,15 @@ export default function WebDesign() {
             and bring memorable brand experiences.
       </DesignHeading>
       <DesignCards images={images} titles={designTitles} messages={designMessages}/>
+      <div className="row gy-4 py-2">
+                <div className="col-lg-6">
+                    <HomeCard Large={appLarge} Medium={appMedium} Small={appSmall} title='app design' route='app' />
+                </div>
+                <div className="col-lg-6">
+                    <HomeCard Large={graphic} Medium={graphicMedium} Small={graphicSmall} title='graphic design' route='graphic' />
+                </div>
+      </div>
+      <LetsTalk/>
     </div>
   )
 }
