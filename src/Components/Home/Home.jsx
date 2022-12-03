9159ef582d45f0c265d3c7a74bf921c5d5e70227
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-03 09:47:10 +0200

    add letstalk component to home component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import HomeHeading from '../HomeHeading/HomeHeading';
 import HomeCards from '../HomeCards/HomeCards';
 import HomeResource from '../HomeResource/HomeResource';
+import LetsTalk from '../LetsTalk/LetsTalk';
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <HomeHeading/>
       <HomeCards/>
       <HomeResource/>
+      <LetsTalk/>
     </div>
   )
 }
