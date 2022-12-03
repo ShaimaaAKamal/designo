274676d6f6137925d387c9847c327a8bea458257
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-03 17:08:33 +0200

    adjust spacing

--- src/Components/DesignCards/DesignCards.jsx
+++ src/Components/DesignCards/DesignCards.jsx
@@ -3,7 +3,7 @@ import DesignCard from '../DesignCard/DesignCard';
 
 export default function DesignCards({images,titles,messages}) {
   return (
-    <div className='row py-5 g-5'>
+    <div className='row py-5 g-5 my-3'>
         {
             titles.map((title,index)=> <DesignCard image={images[index]} title={title} message={messages[index]}/>)
         }
