da9dd547307e1855ed1969d514ec9c7dfee6409d
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-03 17:08:47 +0200

    nochanges

--- src/Components/DesignCard/DesignCard.jsx
+++ src/Components/DesignCard/DesignCard.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function DesignCard({image,title,message}) {
   return (
-         <div className="col-lg-4">
-            <div className='row rounded-3 gx-0'>
+         <div className="col-lg-4 ">
+            <div className='row rounded-3 gx-0 h-100  '>
                 <div className="col-lg-12 col-md-6 col-12 ">
-                     <img src={image} alt={`${title} image`} className='w-100 roundedTop' />
+                     <img src={image} alt={`${title} image`} className='w-100 h-100 roundedTop' />
                 </div>
                 <div className="col-lg-12 col-md-6 col-12">
                    <div className='text-center py-4 bgLightPeach roundedBottom h-100 d-md-flex d-lg-block align-items-center justify-content-center text-md-start text-lg-center'>
