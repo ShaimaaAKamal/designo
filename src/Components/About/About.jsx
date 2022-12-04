fffd1fdb8fe405fbaf05abcd1a4c199fc9b09512
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-04 16:54:44 +0200

    implement world talent componrnt

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import AboutHeading from './AboutHeading/AboutHeading';
+import WorldTalent from './WorldTalent/WorldTalent';
 
 export default function About() {
   return (
       <div className='container-lg'>
         <AboutHeading/>
+        <WorldTalent/>
       </div>
   )
 }
