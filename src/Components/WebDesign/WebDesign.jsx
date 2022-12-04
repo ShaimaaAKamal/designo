cc53343ade841210fe3ead9d95f4f62202ad1bde
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-04 08:31:00 +0200

    reorganize code by using design component to remove deplication

--- src/Components/WebDesign/WebDesign.jsx
+++ src/Components/WebDesign/WebDesign.jsx
@@ -1,15 +1,6 @@
 import React from 'react';
-import DesignHeading from '../DesgnHeading/DesignHeading';
-import DesignCards from '../DesignCards/DesignCards';
+import Design from '../Design/Design';
 import importAll from '../../js/images';
-import HomeCard from '../HomeCard/HomeCard';
-import LetsTalk from '../LetsTalk/LetsTalk';
-import appLarge from '../../assets/home/desktop/image-app-design.jpg';
-import graphic from '../../assets/home/desktop/image-graphic-design.jpg';
-import appMedium from '../../assets/home/tablet/image-app-design.jpg';
-import graphicMedium from '../../assets/home/tablet/image-graphic-design.jpg';
-import appSmall from '../../assets/home/mobile/image-app-design.jpg';
-import graphicSmall from '../../assets/home/mobile/image-graphic-design.jpg';
 
 export default function WebDesign() {
    const designTitles=['EXPRESS','TRANSFER','PHOTON','BUILDER','BLOGR','CAMP']
@@ -17,22 +8,9 @@ export default function WebDesign() {
                         ,'Connects users with local contractors based on their location','Blogr is a platform for creating an online blog or publication',
                          'Get expert training in coding, data, design, and digital marketing'];
   const images = importAll(require.context('../../assets/web-design/desktop', false, /\.(jpg)$/));
+  const msg=`We build websites that serve as powerful marketing tools 
+            and bring memorable brand experiences.`
   return (
-    <div className='container-lg'>
-      <DesignHeading title='web design'>
-            We build websites that serve as powerful marketing tools 
-            and bring memorable brand experiences.
-      </DesignHeading>
-      <DesignCards images={images} titles={designTitles} messages={designMessages}/>
-      <div className="row gy-4 py-2">
-                <div className="col-lg-6">
-                    <HomeCard Large={appLarge} Medium={appMedium} Small={appSmall} title='app design' route='app' />
-                </div>
-                <div className="col-lg-6">
-                    <HomeCard Large={graphic} Medium={graphicMedium} Small={graphicSmall} title='graphic design' route='graphic' />
-                </div>
-      </div>
-      <LetsTalk/>
-    </div>
+    <Design title='web design' msg={msg} designTitles={designTitles} designMessages={designMessages} images={images}/>
   )
 }
