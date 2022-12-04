f756c846874da274dcdc2de30aa31d577c881d49
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-04 08:27:46 +0200

    implement design routes to get each cards to redirect to another design page

---
+++ src/Components/DesignRouteCards/DesignRouteCards.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomeCard from '../HomeCard/HomeCard';
+
+
+export default function DesignRouteCards({DesignRoutes}) {
+  return (
+     <div className="row gy-4 py-2">
+              {DesignRoutes.map((route,index)=>   <div className="col-lg-6" key={index}>
+                    <HomeCard Large={route.large} Medium={route.medium} Small={route.small} title={route.title} route={route.route} />
+                </div>)}
+      </div>
+  )
+}
