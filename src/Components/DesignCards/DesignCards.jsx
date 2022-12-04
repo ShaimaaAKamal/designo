20fed6c881427d233f76114862a2951ff1714e40
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-04 08:31:59 +0200

    add key so react cn detect virtual dom element

--- src/Components/DesignCards/DesignCards.jsx
+++ src/Components/DesignCards/DesignCards.jsx
@@ -5,7 +5,7 @@ export default function DesignCards({images,titles,messages}) {
   return (
     <div className='row py-5 g-5 my-3'>
         {
-            titles.map((title,index)=> <DesignCard image={images[index]} title={title} message={messages[index]}/>)
+            titles.map((title,index)=> <DesignCard key={index} image={images[index]} title={title} message={messages[index]}/>)
         }
     </div>
   )
