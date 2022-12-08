58743cebe739fdf2c63f625df5e72c35672214c3
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-08 08:15:35 +0200

    adjust textarea placeholder effect as it is not working with react

--- src/Components/Contact/ContactForm/ContactForm.jsx
+++ src/Components/Contact/ContactForm/ContactForm.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function ContactForm() {
+  useEffect(()=>{
+    const message=document.querySelector('#message');
+    message.style.color='#FDBDB3';
+    message.addEventListener('focus',function(){
+      if(message.value === 'Your Message')
+           message.value='';
+    });
+     message.addEventListener('blur',function(){
+      message.style.color=(message.value === 'Your Message') ?'#FDBDB3':'#fff'
+    });
+    message.addEventListener('keyup',function(){
+      if(message.value === ''){
+            message.value='Your Message';
+            message.blur();
+      }
+    });
+  },[])
   return (
     <>
       <div className="mb-3">
@@ -13,7 +30,7 @@ export default function ContactForm() {
       <input type="text" className="form-control form-control-lg" id="phone" name='phone' placeholder="Phone"/>
      </div>
      <div className="form-floating">
-        <textarea className="form-control" placeholder="Your Message"  name='message' id="message" style={{height:'100px'}}></textarea>
+        <textarea className="form-control"  name='message' id="message" style={{height:'100px'}} defaultValue='Your Message' ></textarea>
     </div>
     <div className='text-end mt-4'>
             <button className='darkBtn rounded-2 position-relative learnMoreBtn'>Submit</button>
