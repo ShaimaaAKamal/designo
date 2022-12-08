cfcebf3bed5ac937fc291dab1555d3741e418448
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-12-08 17:32:05 +0200

    handle menu close while navigation

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from '../Navbar/Navbar';
 import FooterComp from '../FooterComp/FooterComp';
 
 export default function Layout() {
+  const location=useLocation();
+  const path=location.pathname
+   useEffect(()=>{
+    const collapse=document.querySelector('.collapse');
+    if(collapse.classList.contains('show'))
+    { const menu=document.querySelector('#menu');
+      const close=document.querySelector('#close');
+      collapse.classList.remove('show');
+      close.classList.add('d-none');
+      menu.classList.remove('d-none');
+     }
+  },[path]);
   return (
     <>
         <Navbar/>
